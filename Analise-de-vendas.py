07903ad0accba8777feb39ae4492d082f3af2498
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-17 00:17:53 -0300

    Update Analise-de-vendas.py
    Importação da biblioteca para ler os arquivos

--- Analise-de-vendas.py
+++ Analise-de-vendas.py
@@ -1,1 +1,6 @@
 # Este projeto analisa as vendas de uma empresa com base nas informações dos arquivos da empresa.
+
+import os
+
+lista_arquivo = os.listdir("C:\Projetos-GIT\Projetos Mentoria\Projeto-de-analise-de-vendas\Vendas")
+print(lista_arquivo)
