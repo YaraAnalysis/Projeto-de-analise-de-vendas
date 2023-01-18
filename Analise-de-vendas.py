61b845155236b3823d465a3efd61ae7215453dca
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-18 00:17:11 -0300

    importando a base de dados

--- Analise-de-vendas.py
+++ Analise-de-vendas.py
@@ -1,6 +1,16 @@
 # Este projeto analisa as vendas de uma empresa com base nas informações dos arquivos da empresa.
 
 import os
+import pandas as pd
 
-lista_arquivo = os.listdir("C:\Projetos-GIT\Projetos Mentoria\Projeto-de-analise-de-vendas\Vendas")
-print(lista_arquivo)
+lista_arquivos = os.listdir("C:\Projetos-GIT\Projetos Mentoria\Projeto-de-analise-de-vendas\Vendas")
+print(lista_arquivos)
+
+tabela_total = pd.DataFrame()
+
+for arquivo in lista_arquivos:
+    if 'vendas' in arquivo.lower():
+        #importar arquivo
+        print(f'/content/drive/MyDrive/2. Curso Hashtag Treinamentos/Curso Básico de Python/Vendas/{arquivo}')
+
+print(tabela_total)
